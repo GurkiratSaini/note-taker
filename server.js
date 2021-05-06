14db72626b09f4fe4d10cf9804dd620e9b1360f5
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-05-06 09:01:11 -0400

    set up express

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static(`public`));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server now running on port ${PORT}`);
+});
