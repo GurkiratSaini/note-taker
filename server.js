f74c417fdca90d581252395f2648ce7010b474ba
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-05-07 10:18:38 -0400

    added get routes for serving static pages

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
-
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static(`public`));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 
 
