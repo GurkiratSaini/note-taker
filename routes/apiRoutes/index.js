59c266d783370eb284ace8531459e08b41ea3612
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-05-09 10:23:48 -0400

    finished app

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -6,7 +6,6 @@ const path = require('path');
 apiRouter.get('/api/notes', (req, res) => {
     fs.readFile(path.join(__dirname, '../../db/db.json'), 'utf8', (err, file) => {
         if (err) throw err;
-
         return res.send(JSON.parse(file));
     })
 });
@@ -20,9 +19,7 @@ apiRouter.post('/api/notes', (req, res) => {
 
     fs.readFile(path.join(__dirname, '../../db/db.json'), 'utf8', (err, file) => {
         if (err) throw err;
-
         const storedFile = JSON.parse(file);
-
         storedFile.push(newNote);
 
         fs.writeFile(path.join(__dirname, '../../db/db.json'), JSON.stringify(storedFile), 'utf8', (err) => {
@@ -31,7 +28,21 @@ apiRouter.post('/api/notes', (req, res) => {
         });
         return res.send(storedFile);
     });
+});
 
+apiRouter.delete('/api/notes/:id', (req, res) => {
+    fs.readFile(path.join(__dirname, '../../db/db.json'), 'utf8', (err, file) => {
+        if (err) throw err;
+        let requestId = req.params.id;
+        const storedFile = JSON.parse(file);
+        const fileAfterDelete = storedFile.filter(elem => elem.id != requestId);
+
+        fs.writeFile(path.join(__dirname, '../../db/db.json'), JSON.stringify(fileAfterDelete), 'utf8', (err) => {
+            if (err) throw err;
+            console.log('Note deleted!');
+        });
+        return res.send(fileAfterDelete);
+    });
 });
 
 module.exports = apiRouter;
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const htmlRouter = require('./routes/htmlRoutes/index');
 const apiRouter = require('./routes/apiRoutes/index');
 
